cc0d721199a47571b29604d6d11f3f64888b23cb
DEV04 <dev04@ADSI0225.local>
2022-03-03 10:27:50 +0800

    dependencies setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,49 +1,85 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'movie-database-cms',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: 'Movie Database - Admin',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.png' }
     ]
   },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: {
+      color: '#0084B0',
+      throttle: 0,
+      height: '4px'
+  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
+  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    '~/assets/stylus/app.styl'
   ],
+
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/v-select.js', ssr: false },
+    { src: '~/plugins/vee-validate' },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: false,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/dotenv'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/auth'
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+  auth: {
+    strategies: {
+      local: {
+        token: {
+          property: 'token',
+          global: true,
+          // required: true,
+          // type: 'Bearer'
+        },
+        user: {
+          property: 'user',
+          // autoFetch: true
+        },
+        endpoints: {
+          login: { url: '/users/sign_in', method: 'post', propertyName: 'token' },
+          logout: { url: '/users/sign_out', method: 'delete' },
+          user: { url: '/users/current', method: 'get', propertyName: 'user' }
+        }
+      }
+    }
   },
 
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+    baseURL: process.env.API_URL,
+    credentials: false
+  },
+  env: {
+    baseUrl: process.env.BASE_URL
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  server: {
+    port: process.env.PORT
   }
 }
